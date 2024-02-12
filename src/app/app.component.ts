7084e03a333b3871c5e38d07012a38843b448466
Sakib Al Hasan <bsse1209@iit.du.ac.bd>
2024-02-12 13:02:14 +0600

    loop inner from complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Level1Component } from './level-1/level-1.component';
 
 @Component({
@@ -11,21 +11,13 @@ import { Level1Component } from './level-1/level-1.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
   title = 'Reactive form';
 
-  constructor(private formBuilder: FormBuilder){}
-  simpleForm : FormGroup = new FormGroup('')
-
-  ngOnInit(){
-    this.simpleForm = this.formBuilder.group({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      form: this.formBuilder.group({firstName: new FormControl()})
-    })
-
-
-  }
+  simpleForm = new FormGroup({
+    firstName: new FormControl(),
+    lastName: new FormControl()
+  })
 
   submit(){
     console.log("Form value: ",this.simpleForm.value);
