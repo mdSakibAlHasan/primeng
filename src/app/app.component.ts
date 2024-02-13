68ec837d8c343447f05b9a5bd88b28695f88ab18
Sakib Al Hasan <bsse1209@iit.du.ac.bd>
2024-02-13 13:45:30 +0600

    atleast work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { DragComponent } from './drag/drag.component';
+import { DynamicHooksModule, HookParserEntry, OutletService } from 'ngx-dynamic-hooks';
+import { DynamicComponent } from './dynamic/dynamic.component';
+import { ParserEntryResolver } from 'ngx-dynamic-hooks/lib/components/outlet/options/parserEntryResolver';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, DragComponent],
+  imports: [CommonModule, DynamicHooksModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent{
   title = 'Drag and drop';
+ 
+  componentName = "<app-drag></app-drag>"
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { DynamicHooksModule, HookParserEntry } from 'ngx-dynamic-hooks';
+import { DragComponent } from './drag/drag.component';
+
+const componentParsers: Array<HookParserEntry> = [
+  {component: DragComponent},
+];
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()),importProvidersFrom(BrowserAnimationsModule)]
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()),importProvidersFrom(BrowserAnimationsModule),
+    importProvidersFrom(DynamicHooksModule.forRoot({globalParsers: componentParsers}) )
+  ]
 };
