4d2068f453feb7c74d089eddcdf003e757f5baeb
mdSakibAlHasan <bsse1209@iit.du.ac.bd>
2024-02-16 18:56:54 +0600

    signal with dropdown

--- src/app/child-2/child-2.component.ts
+++ src/app/child-2/child-2.component.ts
@@ -1,17 +1,38 @@
 import { Component, Input, OnInit, SimpleChanges, effect } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
 
 @Component({
   selector: 'app-child-2',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, DropdownModule],
   templateUrl: './child-2.component.html',
   styleUrl: './child-2.component.css'
 })
-export class Child2Component {
+export class Child2Component implements OnInit{
   @Input() data: any
  
-  changeData(changeValue: string){
-    this.data.set(changeValue);
+
+  cities: any;
+
+  selectedCity:any;
+
+  ngOnInit() {
+      this.cities = [
+          { name: 'New York', code: 'NY' },
+          { name: 'Rome', code: 'RM' },
+          { name: 'London', code: 'LDN' },
+          { name: 'Istanbul', code: 'IST' },
+          { name: 'Paris', code: 'PRS' }
+      ];
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(this.selectedCity," ", changes);
+  }
+
+  changeData(){
+    console.log(this.selectedCity)
+    this.data.set(this.selectedCity.name);
   }
 }
